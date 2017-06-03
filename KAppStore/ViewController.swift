fce347f6d0fdeb4604a5a74628a5c22952a2500b
aney <yenafirst91@gmail.com>
2017-06-03 17:11:32 +0900

    인기차트 테이블 뷰 추가

--- KAppStore/ViewController.swift
+++ KAppStore/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+  // MARK: UI
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  // MARK: View Life Cycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.navigationItem.title = "금융"
+    
+    self.tableView.dataSource = self
+    self.tableView.delegate = self
+    
   }
 
   override func didReceiveMemoryWarning() {
@@ -23,3 +35,32 @@ class ViewController: UIViewController {
 
 }
 
+
+// MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 7
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+    
+    cell.backgroundColor = .gray
+    
+    return cell
+  }
+  
+}
+
+
+// MARK: - UITableViewDelegate
+
+extension ViewController: UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print("\(indexPath.row) selected ")
+  }
+  
+}
